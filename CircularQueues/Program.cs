185f73a11434f90b48b6c39d95fcb07b5fbaa1c9
Muhammad Arfan <muhammad.arfan.ft21@mail.umy.ac.id>
2022-12-12 16:39:40 +0700

    membuat public program

--- CircularQueues/Program.cs
+++ CircularQueues/Program.cs
@@ -6,11 +6,17 @@ using System.Threading.Tasks;
 
 namespace CircularQueues
 {
-     class Program
+    class Program
     {
         int FRONT, REAR, max = 5;
         int[] queue_array = new int[5];
 
+        public Program()
+        {
+           FRONT = -1;
+           REAR  = -1;
+
+        }
         static void Main(string[] args)
         {
         }
