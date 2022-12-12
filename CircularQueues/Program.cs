00a71bddd77dc69cfd16bff563983ead12831a61
Muhammad Arfan <muhammad.arfan.ft21@mail.umy.ac.id>
2022-12-12 16:36:08 +0700

    Add project files.

---
+++ CircularQueues/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularQueues
+{
+     class Program
+    {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
